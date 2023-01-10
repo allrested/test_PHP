9ed1e029e398007f310867bc84b749b13c570beb
tatsuya <allrested@gmail.com>
2023-01-10 16:47:45 +0700

    Solution for number four word pattern - hard

--- 4pattern-hard.php
+++ 4pattern-hard.php
@@ -1,25 +1,26 @@
 <?php
 
-function wordPattern(string $pattern, string $str ) {
-    
-    $guide = ["a" => 0, "b" => 1, "c" => 2, "d" => 3];
-    $words = explode(" ", $str);
-    $count_word = array_count_values($words);
-    if(count($words) != strlen($pattern)) {
+function wordPattern( string $pattern, string $str ) {
+
+    $guide = [ 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7, 'i' => 8, 'j' => 9 ];
+    $words = explode( ' ', $str );
+    $count_word = array_count_values( $words );
+    if ( count( $words ) != strlen( $pattern ) ) {
         return false;
     }
 
     $result = true;
-    $unique_words = array_values(array_unique($words));
-    $arr = str_split($pattern);
+    $unique_words = array_values( array_unique( $words ) );
+    $arr = str_split( $pattern );
 
-    for ($i = 0; $i < strlen($pattern); $i++) {
-        if(!isset($guide[$arr[$i]])) {
+    for ( $i = 0; $i < strlen( $pattern );
+    $i++ ) {
+        if ( !isset( $guide[ $arr[ $i ] ] ) ) {
             $result = false;
             break;
         }
 
-        if($words[$i] != $unique_words[$guide[$arr[$i]]]){
+        if ( $words[ $i ] != $unique_words[ $guide[ $arr[ $i ] ] ] ) {
             $result = false;
             break;
         }
@@ -27,6 +28,6 @@ function wordPattern(string $pattern, string $str ) {
     return $result;
 }
 
-dd( wordPattern( "abba", "hello world world hello" ) );
+dd( wordPattern( 'abba', 'hello world world hello' ) );
 
 ?>
