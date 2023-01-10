78d7174524b7f2d0f8af0e733aeb3b46ed34b8b1
tatsuya <allrested@gmail.com>
2023-01-10 16:45:35 +0700

    Solution for number four word pattern - hard

---
+++ 4pattern-hard.php
@@ -0,0 +1,32 @@
+<?php
+
+function wordPattern(string $pattern, string $str ) {
+    
+    $guide = ["a" => 0, "b" => 1, "c" => 2, "d" => 3];
+    $words = explode(" ", $str);
+    $count_word = array_count_values($words);
+    if(count($words) != strlen($pattern)) {
+        return false;
+    }
+
+    $result = true;
+    $unique_words = array_values(array_unique($words));
+    $arr = str_split($pattern);
+
+    for ($i = 0; $i < strlen($pattern); $i++) {
+        if(!isset($guide[$arr[$i]])) {
+            $result = false;
+            break;
+        }
+
+        if($words[$i] != $unique_words[$guide[$arr[$i]]]){
+            $result = false;
+            break;
+        }
+    }
+    return $result;
+}
+
+dd( wordPattern( "abba", "hello world world hello" ) );
+
+?>
